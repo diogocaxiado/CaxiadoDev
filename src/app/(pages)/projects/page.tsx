88b1b8f521ade935283cb5c12c4307d6e462ecc1
Diogo Caxiado <caxiadodev@gmail.com>
2024-05-02 19:21:57 -0300

    feat: Pagina de projetos c/responsividade

--- src/app/(pages)/projects/page.tsx
+++ src/app/(pages)/projects/page.tsx
@@ -2,35 +2,55 @@
 
 import useDataContext from "@/hooks/useDataContext";
 
-import { Project } from "@/components/Project/Project";
+import { Slider } from "@/components/Slider/Slider";
 
 import styles from './page.module.css';
+import { RotateLoader } from "react-spinners";
+import Image from "next/image";
 
 export default function Projects() {
-   const { data } = useDataContext();
+   const { data, isLoading } = useDataContext();
 
-   return (
-      <div>
-          <div className={styles.projects} id='projetos'>
-            <h2 className={styles.title}>Projetos</h2>
-
-            <section className={styles.cards}>
-            {data?.project.map((project) => {
-               const formatTags = project.tags.join(' - ');
-               
-               return (
-                  <Project
-                     key={project.id}
-                     image={project.image}
-                     title={project.name}
-                     description={project.description}
-                     tags={formatTags}
-                     link={project.link}
+  return isLoading || !data ? (
+      <>
+        <head>
+          <title>Loading...</title> 
+        </head>
+        <section className={styles.loading}>
+          <RotateLoader color='var(--yellow)' />
+        </section>
+      </>
+    ) : (
+      <>
+         <head>
+            <title>Caxo Dev - Projetos</title> 
+            <meta property="og:title" content="My page title" key="title" />
+         </head>
+     
+         <main className={styles.main}>
+            <div className={styles.border}>
+               <div className={styles.image}>
+                  <Image
+                     src={data!.avatarProjects}
+                     width={761}
+                     height={761}
+                     alt='Imagem de um avatar apontando para a esquerda em direção ao carousel de projetos'
+                     className={styles.avatar}
                   />
-               );
-            })}
-            </section>
-         </div>
-      </div>
+               </div>
+
+               <div className={styles.info}>
+                  <h2 className={styles.title}>Projetos</h2>
+                  <span className={styles.description}>Explore livremente os projetos</span>
+
+                  <div className={styles.line} />
+
+                  <section className={styles.component}>
+                     <Slider />
+                  </section>
+               </div>
+            </div>
+         </main>
+      </>
    )
 }
